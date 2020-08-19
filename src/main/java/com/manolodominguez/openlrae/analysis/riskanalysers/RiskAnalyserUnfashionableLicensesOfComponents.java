c903b061269288ead6a7db19dd7fc0e3f46b7558
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-08-19 19:06:42 +0200

    Risk analyser has been enhanced and documented

--- src/main/java/com/manolodominguez/openlrae/analysis/riskanalysers/RiskAnalyserUnfashionableLicensesOfComponents.java
+++ src/main/java/com/manolodominguez/openlrae/analysis/riskanalysers/RiskAnalyserUnfashionableLicensesOfComponents.java
@@ -24,13 +24,12 @@ import com.manolodominguez.openlrae.arquitecture.ComponentBinding;
 
 /**
  * This class implements a risk analyser whose mission is to detect those
- * elements whose licenses are not used by many opensource projects. It is
- * desiderable that all components used in a project uses a license that is also
- * used in lot of projects. This way if you need to change a component by
- * another one that provide a given functionality should find out a component
- * that provides that functionality, with the same license, easily. Unless all
- * components of the bill of components are using a wide spreaded license, there
- * are certain level of risk.
+ * elements whose licenses are not trendy (its use is growing). It is
+ * desiderable that all components used in a project uses a license that is
+ * trendy. This way if you need to adda a component with the same license in the
+ * future, to get a new functionality, you can do it easily. Unless all
+ * components of the bill of components are using a trendy license, there are
+ * certain level of risk.
  *
  * We will use the totalCases as the reference point to compute risk exposure
  * and risk impact. totalCases is the number of component bindings that composes
@@ -39,51 +38,86 @@ import com.manolodominguez.openlrae.arquitecture.ComponentBinding;
  * The important is computed this way:
  *
  * riskExposure = average of number of components in the project whose license
- * are wide spreaded, multiplied, each one of them, by its relative weight in
- * the overall project.
+ * are not trendy, multiplied, each one of them, by its relative weight in the
+ * overall project.
  *
- * riskImpact = average of the spreading value of each components in the project
- * whose license are not wide spreaded, multiplied, each one of them by its
- * relative weight in the overall project.
+ * riskImpact = average of the trend value of each components in the project
+ * whose license are not trendy, multiplied, each one of them by its relative
+ * weight in the overall project.
  *
- * riskExposure should be undestood as the portion of the project that is 
+ * riskExposure should be undestood as the portion of the project that is
  * affected by the risk. riskImpact should be undestood as the effort needed to
  * reduce the risk exposure (think in riskImpact in cost terms).
- * 
+ *
  * @author Manuel Domínguez Dorado
  */
 public class RiskAnalyserUnfashionableLicensesOfComponents extends AbstractRiskAnalyser {
 
+    /**
+     * This is the constructor of the class. It creates a new instance of
+     * RiskAnalyserUnfashionableLicensesOfComponents.
+     *
+     * @param project. The software project to be analised.
+     */
     public RiskAnalyserUnfashionableLicensesOfComponents(Project project) {
         super(project, SupportedRisks.UNFASHIONABLE_LICENSES_OF_COMPONENTS, RiskAnalyserUnfashionableLicensesOfComponents.class);
     }
 
+    /**
+     * This method analyse the project and its components looking for risk of
+     * using components whose license are not trendy.
+     *
+     * It is better to use components whose license is trendy (is being used
+     * more and more in third party projects) in order to have the possibility
+     * to choose third party components in teh future, easily, without licensing
+     * risks. The overall bill of components of the project is analyzed and a
+     * global risk is computed.
+     *
+     * @return the result of the analysis.
+     */
     @Override
     public RiskAnalysisResult getRiskAnalisysResult() {
         reset();
-        
+
         SupportedTrends trend;
         LicensesTrendFactory licensesTrends = LicensesTrendFactory.getInstance();
-        
+        int totalCases = this.project.getComponentsBindings().size();
+
         for (ComponentBinding componentBinding : this.project.getComponentsBindings()) {
             trend = licensesTrends.getTrendOf(componentBinding.getComponent().getLicense());
             switch (trend) {
                 case TRENDY:
-                    // Nothing to do. There's no risk
+                    // The analyzed component is using a license that is trendy
+                    // (is being used more and more in third party projects). 
+                    // Therefore there is not unfashionable license risk in this
+                    // case. 
+                    goodThings.add(componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " uses a license (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") " + "that is trendy (the use of this license is growing).");
                     break;
                 case NEAR_TRENDY:
+                    // The analyzed component is using a license that is not 
+                    // completely trendy (is being used more and more in third 
+                    // party projects but slowly). Therefore there is 
+                    // unfashionable license risk in this case. 
                     rootCauses.add(componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " has a license (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") that is " + licensesTrends.getTrendOf(componentBinding.getComponent().getLicense()).toString() + " in terms of trend");
                     tips.add("Try changing " + componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") by another component released under a license more trendy.");
                     riskImpact += (trend.getTrendValue() * componentBinding.getWeight().getWeightValue());
                     riskExposure += componentBinding.getWeight().getWeightValue();
                     break;
                 case NEAR_UNFASHIONABLE:
+                    // The analyzed component is using a license that is not 
+                    // trendy (is being used less and less in third party 
+                    // projects but slowly). Therefore there is unfashionable 
+                    // license risk in this case. 
                     rootCauses.add(componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " has a license (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") that is " + licensesTrends.getTrendOf(componentBinding.getComponent().getLicense()).toString() + " in terms of trend");
                     tips.add("Try changing " + componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") by another component released under a license more trendy.");
                     riskImpact += (trend.getTrendValue() * componentBinding.getWeight().getWeightValue());
                     riskExposure += componentBinding.getWeight().getWeightValue();
                     break;
                 case UNFASHIONABLE:
+                    // The analyzed component is using a license that is not 
+                    // trendy at all (is poorly used or is being used less and 
+                    // less in third party projects but very fast). Therefore 
+                    // there is unfashionable license risk in this case. 
                     rootCauses.add(componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " has a license (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") that is " + licensesTrends.getTrendOf(componentBinding.getComponent().getLicense()).toString() + " in terms of trend");
                     tips.add("Try changing " + componentBinding.getComponent().getName() + "-" + componentBinding.getComponent().getVersion() + " (" + componentBinding.getComponent().getLicense().getShortNameValue() + ") by another component released under a license more trendy.");
                     riskImpact += (trend.getTrendValue() * componentBinding.getWeight().getWeightValue());
@@ -92,8 +126,8 @@ public class RiskAnalyserUnfashionableLicensesOfComponents extends AbstractRiskA
             }
         }
 
-        riskExposure = riskExposure / (float) this.project.getComponentsBindings().size();
-        riskImpact = riskImpact / (float) this.project.getComponentsBindings().size();
+        riskExposure /= (float) totalCases;
+        riskImpact /= (float) totalCases;
         if (riskExposure > 0.0f) {
             tips.add("When modifying the project set of components to reduce the exposure to this risk, start changing components that are root causes in more cases.");
             tips.add("When modifying the project set of components to reduce the exposure to this risk, start with those with higher level of contribution to the overall project.");
