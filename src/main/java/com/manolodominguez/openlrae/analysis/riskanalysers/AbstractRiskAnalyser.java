a874a83010ab534b3a5981ff245a46c5ccee69ac
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-08-20 18:39:18 +0200

    Class has been enhanced and documented

--- src/main/java/com/manolodominguez/openlrae/analysis/riskanalysers/AbstractRiskAnalyser.java
+++ src/main/java/com/manolodominguez/openlrae/analysis/riskanalysers/AbstractRiskAnalyser.java
@@ -23,8 +23,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * This abstract class has to be implemented by every risk analysers. It defines
+ * common behaviour and several common attributes that, again, every
+ * implementing class has to use.
  *
- * @author manolodd
+ * @author Manuel Domínguez Dorado
  */
 public abstract class AbstractRiskAnalyser {
 
@@ -38,6 +41,15 @@ public abstract class AbstractRiskAnalyser {
     protected CopyOnWriteArrayList<String> goodThings;
     protected CopyOnWriteArrayList<String> tips;
 
+    /**
+     * This is the constructor of the class.It has to be called from within the
+     * subclass constructyor.It configure attributes default values.
+     *
+     * @param project The software project to be analised.
+     * @param handledRiskType The type of risk the subclass addresses.
+     * @param logForClass The subclass that implements this abstract class. It
+     * is used for loggin purposes.
+     */
     public AbstractRiskAnalyser(Project project, SupportedRisks handledRiskType, Class<?> logForClass) {
         this.project = project;
         this.handledRiskType = handledRiskType;
@@ -50,6 +62,9 @@ public abstract class AbstractRiskAnalyser {
         tips = new CopyOnWriteArrayList<>();
     }
 
+    /**
+     * This private method resets the instace as in the moment it was created.
+     */
     private void reset() {
         riskExposure = AbstractRiskAnalyser.DEFAULT_EXPOSURE_LEVEL;
         riskImpact = AbstractRiskAnalyser.DEFAULT_IMPACT_LEVEL;
@@ -59,17 +74,61 @@ public abstract class AbstractRiskAnalyser {
         tips.clear();
     }
 
-    public SupportedRisks getHandledRiskType() {
-        return handledRiskType;
-    }
-
-    protected abstract void runAnalyser();
-    
+    /**
+     * This method resets the instace, run the analysis and return the
+     * corresponding risk analysis result.
+     *
+     * @return the result of running the risk analysis.
+     */
     public RiskAnalysisResult getRiskAnalisysResult() {
         reset();
         runAnalyser();
         return new RiskAnalysisResult(handledRiskType, riskExposure, riskImpact, rootCauses, warnings, goodThings, tips);
     }
+
+    /**
+     * This method gets the kind of risk addressed by the risk analyser.
+     *
+     * @return the kind of risk addressed by the risk analyser.
+     */
+    public SupportedRisks getHandledRiskType() {
+        return handledRiskType;
+    }
+
+    /**
+     * This is the abstract method that has to be implemented in every subclass
+     * developed to address a given risk. This class makes available to the
+     * developer a set of attributes he/she has to use for the risk analyzer to
+     * be operational.
+     *
+     * Important information for developers of risk analysers:
+     *
+     * - At the beggining of the method all internal values are always as if the
+     * intances is newly created.
+     *
+     * - He/se has to use rootCauses, warnings and goodThings attributes
+     * wherever possible to provide a comprehensive analysis of whatever the
+     * risk analyser is suppose to analyse. rootCauses is used to give
+     * information about causes that determine a given thing is a risk (thin as
+     * RED in a semaphore). Warnings is used to give information of interest
+     * about situations the user has to know (think as ORANGE in a semaphore).
+     * goodThings is used to give information about things that are OK and not
+     * suppose an increment in the risk level (think as GREEN in a semaphore).
+     * These three attributes can be used in the risk analyser multiple times to
+     * provide a rich analysis. They are the "risk report" consisting in a set
+     * of messages that allow the user to identify root causes of risks, special
+     * situations or things well done.
+     *
+     * - He/she has to update riskExposure and riskImpact attributes before the
+     * end of runAnalyser() method. These values are the risk computation. Both
+     * values are always between 0.00 (0%) and 1.00 (100%) as float values. It
+     * is important to maintain the same semantics in all implemented risk
+     * analysers to avoid misinterpretation of results. So, riskExposure has to
+     * measure the portion of the project that is affected by a given risk. And
+     * riskImpact has to measure the "effort" or "cost" needed to make the
+     * project free of this riskExposure.
+     */
+    protected abstract void runAnalyser();
 
     private static final float DEFAULT_EXPOSURE_LEVEL = 0.0f;
     private static final float DEFAULT_IMPACT_LEVEL = 0.0f;
