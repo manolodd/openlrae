d81319fdb220472a0fc8e36fe86e52fcf2fd0165
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-09-01 17:09:40 +0200

    Fix args handling

--- src/main/java/com/manolodominguez/openlrae/analysis/main/MainClass.java
+++ src/main/java/com/manolodominguez/openlrae/analysis/main/MainClass.java
@@ -64,8 +64,8 @@ public class MainClass {
                     break;
             }
         } else {
-            //showOptions();
-            runExample();
+            showOptions();
+            //runExample();
         }
     }
 
