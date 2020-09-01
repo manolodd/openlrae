f3afc22bb32883556a02d859a2cc94701267db1c
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-09-01 17:59:36 +0200

    Class has been enhanced and documented

--- src/main/java/com/manolodominguez/openlrae/baseofknowledge/licenseproperties/licensecompatibilities/LicenseCompatibilityEntry.java
+++ src/main/java/com/manolodominguez/openlrae/baseofknowledge/licenseproperties/licensecompatibilities/LicenseCompatibilityEntry.java
@@ -23,6 +23,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * This class implements a container for a license compatibility combination
+ * knowledge. To be specific, this determines if a component with a given
+ * license can be included in a project with its own license, taking into
+ * account a specific binding way and the distribution of the project. Things
+ * like, for instance: "Can a component under GPL-2.0-only be included
+ * DINAMICALLY in a project that is going to be redistributed as SOFTWARE
+ * PACKAGE under the terms of BSD-3-Clause license?" This specific knowledge
+ * pill has to be encoded in the base of knowledge of OpenLRAE within an
+ * instance of this class.
  *
  * @author Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com
  */
@@ -35,15 +44,34 @@ public class LicenseCompatibilityEntry {
     private SupportedCompatibilities compatibility;
     private SupportedLinks link;
     private SupportedRedistributions redistribution;
-    private String explanation;
-    
-    public LicenseCompatibilityEntry(SupportedLicenses componentLicense, SupportedLicenses projectLicense, SupportedCompatibilities compatibility, SupportedLinks link, SupportedRedistributions redistribution, String explanation) {
+    private String specificWarning;
+
+    /**
+     * This is the constructor of the class. It creates a new instance of
+     * LicenseCompatibilityEntry and initialize its attributes with the specific
+     * information to solve one particular case of licensing compatibility.
+     *
+     * @param componentLicense The license of a component.
+     * @param projectLicense The license of a project where the component is
+     * going to be (or it is) included.
+     * @param compatibility The type of compatibility that applies to this case
+     * taking into account the value of the rest of attributes. This is how the
+     * knowledge is included in OpenLRAE. 
+     * @param link The way the component is going to be included (or its is
+     * included) in the prtoject.
+     * @param redistribution The kind of project distribution.
+     * @param specificWarning If the is any additional hint related to this
+     * specific licene compatiliblity combination, it should be put here so taht
+     * different risk analysers can use it properly. If ther is no hints to
+     * highlight, null should be specified.
+     */
+    public LicenseCompatibilityEntry(SupportedLicenses componentLicense, SupportedLicenses projectLicense, SupportedCompatibilities compatibility, SupportedLinks link, SupportedRedistributions redistribution, String specificWarning) {
         this.componentLicense = componentLicense;
         this.projectLicense = projectLicense;
         this.compatibility = compatibility;
         this.link = link;
         this.redistribution = redistribution;
-        this.explanation = explanation;
+        this.specificWarning = specificWarning;
     }
 
     public SupportedLicenses getComponentLicense() {
@@ -67,10 +95,10 @@ public class LicenseCompatibilityEntry {
     }
 
     public String getSpecificWarning() {
-        return explanation;
+        return specificWarning;
     }
-    
+
     public boolean hasSpecificWarning() {
-        return this.explanation != null;
+        return this.specificWarning != null;
     }
 }
