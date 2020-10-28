4b274e9f23c9cb716d1f33a69a11deacde6ffc9f
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-10-28 20:57:01 +0100

    More unit tests

---
+++ src/test/java/com/manolodominguez/openlrae/analysis/riskanalysers/RiskAnalyserUnfashionableProjectLicensesTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 manolodd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.manolodominguez.openlrae.analysis.riskanalysers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author manolodd
+ */
+public class RiskAnalyserUnfashionableProjectLicensesTest {
+    
+    public RiskAnalyserUnfashionableProjectLicensesTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of runAnalyser method, of class RiskAnalyserUnfashionableProjectLicenses.
+     */
+    @Test
+    public void testRunAnalyser() {
+        System.out.println("runAnalyser");
+        RiskAnalyserUnfashionableProjectLicenses instance = null;
+        instance.runAnalyser();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
