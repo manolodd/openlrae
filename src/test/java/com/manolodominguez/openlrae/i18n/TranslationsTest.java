4e9619a361803a73a44dc521d7a630f88110df26
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2020-11-10 14:23:37 +0100

    Minor changes

--- src/test/java/com/manolodominguez/openlrae/i18n/TranslationsTest.java
+++ src/test/java/com/manolodominguez/openlrae/i18n/TranslationsTest.java
@@ -55,8 +55,8 @@ public class TranslationsTest {
     @Test
     public void testItemsNumber() {
         System.out.println("items number");
-        // Currently there are 9 resource bundles defined in enum
-        assertEquals(9, Translations.values().length);
+        // Currently there are 10 resource bundles defined in enum
+        assertEquals(10, Translations.values().length);
     }
 
     /**
@@ -79,6 +79,7 @@ public class TranslationsTest {
                 case SUPPORTED_LINKS:
                 case SUPPORTED_OBSOLESCENCES:
                 case SUPPORTED_REDISTRIBUTIONS:
+                case SUPPORTED_RISKS:
                     worksFine &= true;
                     break;
                 default:
