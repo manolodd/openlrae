f6eecb5b56168a37deae672d2e0d2bd9e19fa06a
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2021-01-31 12:38:22 +0100

    Preparing release 1.3

--- src/test/java/com/manolodominguez/openlrae/analysis/riskanalysers/RiskAnalyserLimitedSetOfPotentialProjectLicensesTest.java
+++ src/test/java/com/manolodominguez/openlrae/analysis/riskanalysers/RiskAnalyserLimitedSetOfPotentialProjectLicensesTest.java
@@ -124,13 +124,13 @@ class RiskAnalyserLimitedSetOfPotentialProjectLicensesTest {
         assertTrue(!result.getTips().isEmpty());
         assertTrue(result.getRiskExposure() >= 0.0f);
         assertTrue(result.getRiskExposure() <= 1.0f);
-        assertEquals(0.9524f, result.getRiskExposure());
+        assertEquals(0.9091f, result.getRiskExposure());
         assertTrue(result.getRiskImpact() >= 0.0f);
         assertTrue(result.getRiskImpact() <= 1.0f);
-        assertEquals(0.3965f, result.getRiskImpact());
+        assertEquals(0.3633f, result.getRiskImpact());
         assertTrue(result.getRiskValue() >= 0.0f);
         assertTrue(result.getRiskValue() <= 1.0f);
-        assertEquals(0.3776f, result.getRiskValue());
+        assertEquals(0.3303f, result.getRiskValue());
     }
     
     /**
